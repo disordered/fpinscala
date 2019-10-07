76a94b3804121d1d896dafbf2e6fa891045bdef5
Emilis Panovas <emilis.panovas@gmail.com>
2019-10-07 13:18:37 +0200

    Implemented List.zipWith

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -137,4 +137,12 @@ object List { // `List` companion object. Contains functions for creating and wo
       case (l, Nil) => l
     }
   }
+
+  def zipWith[A, B, C](l1: List[A], l2: List[B])(f: (A, B) => C): List[C] = {
+    (l1, l2) match {
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1, h2), zipWith(t1, t2)(f))
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+    }
+  }
 }
