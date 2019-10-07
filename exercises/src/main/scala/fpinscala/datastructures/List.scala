e451fd58e513bb33a7bbd6b33a8700655bc6bb51
Emilis Panovas <emilis.panovas@gmail.com>
2019-10-07 12:57:04 +0200

    Implemented List.flatMap

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -121,4 +121,8 @@ object List { // `List` companion object. Contains functions for creating and wo
   def filter[A](as: List[A])(f: A => Boolean): List[A] = {
     foldRight(as, Nil: List[A])((h, l) => if (f(h)) Cons(h, l) else l)
   }
+
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] = {
+    concatenate(map(as)(f))
+  }
 }
