780e33a948967e84986ee5c152642fcffb6700f0
Emilis Panovas <emilis.panovas@gmail.com>
2019-11-27 14:54:07 +0100

    Formatted Tree.scala with scalafmt

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -4,27 +4,26 @@ sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
-
 object Tree {
 
   def size[A](tree: Tree[A]): Int = tree match {
-      case Branch(left, right) => size(left) + size(right) + 1
-      case _ => 1
-    }
+    case Branch(left, right) => size(left) + size(right) + 1
+    case _                   => 1
+  }
 
   def maximum(tree: Tree[Int]): Int = tree match {
     case Branch(left, right) => maximum(left) max maximum(right)
-    case Leaf(value) => value
+    case Leaf(value)         => value
   }
 
   def depth[A](tree: Tree[A]): Int = tree match {
     case Branch(left, right) => depth(left) max depth(right) + 1
-    case _ => 0
+    case _                   => 0
   }
 
-  def map[A,B](tree: Tree[A])(f: A => B): Tree[B] = tree match {
+  def map[A, B](tree: Tree[A])(f: A => B): Tree[B] = tree match {
     case Branch(left, right) => Branch(map(left)(f), map(right)(f))
-    case Leaf(value) => Leaf(f(value))
+    case Leaf(value)         => Leaf(f(value))
   }
 
   def main(args: Array[String]) = {
