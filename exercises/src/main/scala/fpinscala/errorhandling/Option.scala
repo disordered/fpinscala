bc4d4c6724c55580ffeef77234f4ac2a3f877a3b
Emilis Panovas <emilis.panovas@gmail.com>
2019-12-27 18:33:57 +0100

    Implemented sequence on Option

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -48,7 +48,14 @@ object Option {
 
   def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = a.flatMap(av => b.map(bv => f(av, bv)))
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
+  def sequence[A](a: List[Option[A]]): Option[List[A]] =
+    a.foldRight[Option[List[A]]](Some(Nil))((x, y) => map2(x, y)(_ :: _))
 
   def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = ???
+
+  def main(args: Array[String]): Unit = {
+    println(sequence(List()))
+    println(sequence(List(Some(1), None: Option[Int])))
+    println(sequence(List(Some(1), Some(2), Some(3))))
+  }
 }
